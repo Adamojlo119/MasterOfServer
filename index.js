e0da124f34ed37620e9cadbdd9bd118dd60a441f
Adamojlo119 <34109905+Adamojlo119@users.noreply.github.com>
2017-12-08 17:07:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ client.on("message", function(message) {
 
 
    //muzykowo
-   if ((message.channel.name === OnlyMusic && message.content.startsWith(MUSIC_PREFIX)) || (message.channel.name === OnlyMusic && message.author.equals(client.user.id === "274543915070390282"))) {
+   if ((message.channel.name === OnlyMusic && message.content.startsWith(MUSIC_PREFIX)) || (message.channel.name === OnlyMusic && message.author.id === "274543915070390282")) {
      return;
    }
    else if (message.channel.name === OnlyMusic) {
